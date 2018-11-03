4a63565306991b4bb751109d152c491ca57a5919
Anthematics <Jasonharder2@gmail.com>
2018-11-03 03:11:38 -0400

    properly displaying top 2 elements

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import './Components/filmListing.js'
+import FilmListing from './Components/filmListing.js'
+import FilmDetails from './Components/filmDetails';
 
 class App extends Component {
   render() {
     return (
       <div className="film-library">
+      <FilmListing />
+      <FilmDetails />
+
     </div>
     );
   }
